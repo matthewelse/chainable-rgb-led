dd13edabbabaefa45d7a913bc6b6276a78da3502
Matthew Else <me390@cam.ac.uk>
2016-09-19 11:52:00 +0100

    Remove jsmbed* function calls

--- chainable-rgb-led/source/ChainableLED.cpp
+++ chainable-rgb-led/source/ChainableLED.cpp
@@ -34,12 +34,13 @@ DECLARE_CLASS_FUNCTION(ChainableLED, setColorRGB)
     CHECK_ARGUMENT_TYPE_ALWAYS(ChainableLED, __constructor, 2, number);
     CHECK_ARGUMENT_TYPE_ALWAYS(ChainableLED, __constructor, 3, number);
 
-    uint32_t led_num = (uint32_t)jsmbed_wrap_unbox_number(args[0]);
-    uint8_t r = (uint8_t)jsmbed_wrap_unbox_number(args[1]);
-    uint8_t g = (uint8_t)jsmbed_wrap_unbox_number(args[2]);
-    uint8_t b = (uint8_t)jsmbed_wrap_unbox_number(args[3]);
+    uint32_t led_num = (uint32_t)jerry_get_number_value(args[0]);
+    uint8_t r = (uint8_t)jerry_get_number_value(args[1]);
+    uint8_t g = (uint8_t)jerry_get_number_value(args[2]);
+    uint8_t b = (uint8_t)jerry_get_number_value(args[3]);
 
-    uintptr_t native_ptr = jsmbed_wrap_get_native_handle(this_obj);
+    uintptr_t native_ptr;
+    jerry_get_object_native_handle(this_obj, &native_ptr);
 
     ((ChainableLED*)native_ptr)->setColorRGB(led_num, r, g, b);
 
@@ -56,11 +57,11 @@ DECLARE_CLASS_CONSTRUCTOR(ChainableLED) {
     CHECK_ARGUMENT_TYPE_ALWAYS(ChainableLED, __constructor, 1, number);
     CHECK_ARGUMENT_TYPE_ALWAYS(ChainableLED, __constructor, 2, number);
 
-    int clk_pin = jsmbed_wrap_unbox_number(args[0]);
-    int sda_pin = jsmbed_wrap_unbox_number(args[1]);
-    int num_leds = jsmbed_wrap_unbox_number(args[2]);
+    int clk_pin = jerry_get_number_value(args[0]);
+    int sda_pin = jerry_get_number_value(args[1]);
+    int num_leds = jerry_get_number_value(args[2]);
 
-    uintptr_t native_ptr = (uintptr_t)new ChainableLED((PinName)clk_pin, (PinName)sda_pin, (PinName)num_leds);
+    uintptr_t native_ptr = (uintptr_t)new ChainableLED((PinName)clk_pin, (PinName)sda_pin, num_leds);
 
     jerry_value_t js_object = jerry_create_object();
     jerry_set_object_native_handle(js_object, native_ptr, NAME_FOR_CLASS_NATIVE_DESTRUCTOR(ChainableLED));
